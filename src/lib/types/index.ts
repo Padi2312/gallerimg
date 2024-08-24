783bc3896e1ab203b1e998d9ccf1ca1224f6fb5d
padi2312 <patrick.arndt.99@web.de>
2024-08-24 18:06:16 +0200

    Removed unused types

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,17 +1,3 @@
-export type ImageItem = {
-    id: string;
-    url: string;
-    title: string;
-    tags: string[];
-    uploadDate: string;
-};
-
-export interface Image {
-    title?: string;
-    description?: string;
-    tags?: string[];
-}
-
 export interface ImageDto {
     id: string;
     url: string;
