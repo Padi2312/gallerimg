e2b9431f75b4c0a8f292a1254d76fe81fd94d447
padi2312 <patrick.arndt.99@web.de>
2024-08-31 18:38:53 +0200

    removed unecessary code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import Gallery from '$lib/components/common/Gallery.svelte';
-	import Image from '$lib/components/common/Image.svelte';
 	import ImageCarousel from '$lib/components/common/ImageCarousel.svelte';
 	import type { ImageDto } from '$lib/types';
 	import Header from '../lib/components/Header.svelte';
@@ -15,12 +14,6 @@
 	<hr class="w-3/4 p-4" />
 </div>
 <div class="relative flex justify-center px-4">
-	<!-- <div class="flex w-full max-w-3xl flex-col items-center space-y-4 overflow-hidden">
-		{#each images as image (image.id)}
-			<Image {image} width={800} showTags />
-		{/each}
-	</div> -->
-
 	<Gallery {images} />
 </div>
 
