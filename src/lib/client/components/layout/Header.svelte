84fa8d9efe1deaab2c8b4f5b9aa67a0ebffd6b98
padi2312 <patrick.arndt.99@web.de>
2024-09-14 23:21:23 +0200

    Refactor header component. made more accessible for mobile view

--- src/lib/client/components/layout/Header.svelte
+++ src/lib/client/components/layout/Header.svelte
@@ -3,44 +3,60 @@
 	import { faBars } from '@fortawesome/free-solid-svg-icons';
 	import { Fa } from 'svelte-fa';
 	import DarkModeToggle from '../common/DarkModeToggle.svelte';
+	import { slide } from 'svelte/transition';
 
-	type HeaderProps = {
-		showToggleSidebar?: boolean;
-		toggleSidebar?: () => void;
-	};
-	let { showToggleSidebar = false, toggleSidebar }: HeaderProps = $props();
+	type HeaderProps = {};
+	let {}: HeaderProps = $props();
 
-	let isHomeUrl = $state(true);
+	let isMenuOpen = $state(true); // State for mobile menu
 
-	$effect(() => {
-		$page.url.pathname === '/' ? (isHomeUrl = true) : (isHomeUrl = false);
-	});
+	function toggleMenu() {
+		isMenuOpen = !isMenuOpen;
+	}
 </script>
 
 <header class="h-16 border-b border-border p-4">
 	<div class="mx-auto flex items-center justify-between space-x-2">
 		<div class="flex items-center">
-			{#if showToggleSidebar}
-				<div class="lg:hidden">
-					<button onclick={toggleSidebar} class="!bg-transparent ps-0">
-						<Fa icon={faBars} class="text-2xl text-text" />
-					</button>
-				</div>
-			{/if}
 			<a href="/">
 				<div class="relative flex items-center space-x-2">
 					<img src="/favicon.png" alt="Gallerimg" class="h-8 w-8" />
 					<h1 class=" text-2xl font-bold">Photo Parndt</h1>
-
-					<!-- <p class="absolute -bottom-3 right-4 text-xs">by parndt</p> -->
 				</div>
 			</a>
-			<a href={isHomeUrl ? '/gallery' : '/'} class="ms-6 hover:underline">
-				<p class="">{isHomeUrl ? 'Gallery' : 'Home'}</p>
-			</a>
 		</div>
-		<div class="float-end">
-			<DarkModeToggle />
+
+		<!-- Desktop Links -->
+		<nav class="hidden space-x-4 md:flex">
+			<a href="/" class="hover:underline">Home</a>
+			<a href="/gallery" class="hover:underline">Gallery</a>
+		</nav>
+		<div class="flex items-center space-x-4">
+			<!-- Dark Mode Toggle -->
+			<div class="float-end">
+				<DarkModeToggle />
+			</div>
+
+			<!-- Mobile Menu Toggle -->
+			<div class="md:hidden">
+				<button onclick={toggleMenu} class="!bg-transparent ps-0">
+					<Fa icon={faBars} class="text-2xl text-text" />
+				</button>
+			</div>
 		</div>
 	</div>
+
+	<!-- Mobile Links -->
+	{#if isMenuOpen}
+		<nav
+			transition:slide
+			class="absolute z-20 mt-4 flex h-full w-full flex-col space-y-2 bg-bg sm:flex md:hidden"
+		>
+			{#snippet link(url: string, text: string)}
+				<a href={url} class="block p-2 font-semibold hover:underline">{text}</a>
+			{/snippet}
+			{@render link('/', 'Home')}
+			{@render link('/gallery', 'Gallery')}
+		</nav>
+	{/if}
 </header>
